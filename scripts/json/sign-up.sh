55d7246e2aa68aa3b1dd85a0847f8ebb11d735d9
quidprocrow <windmillwarrior@gmail.com>
2017-12-21 14:36:30 -0500

    Verify AJAX sign in is working
    Verify AJAX sign in working with curl script

---
+++ scripts/json/sign-up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# sh scripts/json/sign-up.sh
+
+curl "http://tic-tac-toe.wdibos.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials":
+  {
+    "email": "'"${EMAIL}"'",
+    "password": "'"${PASSWORD}"'",
+    "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+  }
+}'
+
+echo
