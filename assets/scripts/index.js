cb9f05b9fe180cbac6594b366e641eefff5e5ddf
quidprocrow <windmillwarrior@gmail.com>
2017-12-21 10:55:43 -0500

    Create event listeners for game board
    Create event listeners for game board that load after the page
    elements, attached to board IDs. Console logs which cell is clicked.
    
    Create turn counter that starts at one, and increases with each click.
    
    If turn counter is odd, the board cell's div HTML is replaced with X;
    else, it is replaced with O.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,8 +3,40 @@
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
 
+let turnCounter = 1
+
+const clickEvent = function () {
+  console.log('Clicked', this.id)
+  const boxId = '#' + this.id
+  $(boxId).css('background', 'pink')
+  console.log('It is the', turnCounter, 'round.')
+  boardMove(boxId)
+  turnCounter++
+}
+
+const boardMove = function (boxId) {
+  const xHtml = (`<p>X</p>`)
+  const oHtml = (`<p>O</p>`)
+  if (turnCounter === 0 || !(turnCounter % 2 === 0)) {
+    $(boxId).html(xHtml)
+    console.log('x')
+  } else {
+    $(boxId).html(oHtml)
+    console.log('o')
+  }
+}
+
 $(() => {
   setAPIOrigin(location, config)
+  $('#0').on('click', clickEvent)
+  $('#1').on('click', clickEvent)
+  $('#2').on('click', clickEvent)
+  $('#3').on('click', clickEvent)
+  $('#4').on('click', clickEvent)
+  $('#5').on('click', clickEvent)
+  $('#6').on('click', clickEvent)
+  $('#7').on('click', clickEvent)
+  $('#8').on('click', clickEvent)
 })
 
 // use require with a reference to bundle the file and use it in this file
