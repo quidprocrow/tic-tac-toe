4a3d97d6474e11dea53e34a13eaf98e7b5ef6562
quidprocrow <windmillwarrior@gmail.com>
2017-12-21 12:55:43 -0500

    Tidy files and add comments
    Tidy files by separating index into index (api configuration and adding
    event handlers to loaded objects) and events (all events initiated by
    the user). Also added a wealth of comments, and eliminated all console
    logs by commenting them out, to be used as needed upon errors.

---
+++ assets/scripts/events.js
@@ -0,0 +1,87 @@
+'use strict'
+
+// TurnCounter used to check for whose move it is (based on even- or uneven-ness)
+// In future work, I would want to make this as a result of a function that checks
+// whether or not the game is reloaded, and if so, would identify the turn
+// by assuming X is the first move and then identifying whose turn it is
+// by whether or not there are more x's than o's on the board.
+
+let turnCounter = 1
+
+// This will be replaced with actual information by the game cell array.
+
+const boardArray = [ '', '', '', '', '', '', '', '', '' ]
+
+// Grabs the 'cell index' information from the div's html to return it.
+const getData = function (box) {
+  return $(box).data('cell-index')
+}
+
+// Makes the board array reflect the move made on the HTML board.
+const setMove = function (move, boardArray, index) {
+  boardArray[index] = move
+  console.log(boardArray)
+}
+
+// Clear any errors from the page's display.
+const msgClear = function () {
+  $('#user-msg').html('')
+}
+
+// Places the move on the HTML board and the array.
+const boardMove = function (boxId) {
+  const xHtml = (`<p>X</p>`)
+  const oHtml = (`<p>O</p>`)
+  const boardCell = getData(boxId)
+  // console.log(boardArray)
+  // console.log(boardCell)
+  //
+  // First, check to see if the array has been filled at the same index as the
+  // data-cell-index of the div that was clicked. If not, proceed. Otherwise,
+  // alert the user.
+  if (boardArray[boardCell] === '') {
+    // console.log('board cell is', boardCell)
+    //
+    // Check to see what turn it is.
+    // If uneven, it's X's turn.
+    if (!(turnCounter % 2 === 0)) {
+      // Put an 'x' on the board.
+      $(boxId).html(xHtml)
+      const move = 'x'
+      // console.log(move)
+      // Put an 'x' in the appropriate spot on the array.
+      setMove(move, boardArray, boardCell)
+      // Increase the turn.
+      turnCounter++
+    } else {
+      // If the number of turns is even...
+      // Put an 'o' on the board.
+      $(boxId).html(oHtml)
+      const move = 'o'
+      // console.log(move)
+      // Put an 'o' in the appropriate spot on the array.
+      setMove(move, boardArray, boardCell)
+      // console.log('o')
+      // Increase the turn.
+      turnCounter++
+    }
+    // Clear any errors from the display.
+    msgClear()
+  } else {
+    // Display error to user that an invalid move has been made.
+    // Importantly: does not update turn, nor the board array.
+    const invalidMoveHtml = (`<B>Someone beat you to it!</B>`)
+    $('#user-msg').html(invalidMoveHtml)
+  }
+}
+
+// This is the event that runs whenever a user clicks a board cell.
+const clickEvent = function () {
+  // console.log('Clicked', this.id)
+  const boxId = '#' + this.id
+  // console.log('It is the', turnCounter, 'round.')
+  boardMove(boxId)
+}
+module.exports = {
+  clickEvent
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,63 +2,20 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const events = require('./events.js')
 
-let turnCounter = 1
-const boardArray = [ '', '', '', '', '', '', '', '', '' ]
-const getData = function (box) {
-  return $(box).data('cell-index')
-}
-const setMove = function (move, boardArray, index) {
-  boardArray[index] = move
-  console.log(boardArray)
-}
-
-const clickEvent = function () {
-  console.log('Clicked', this.id)
-  const boxId = '#' + this.id
-  console.log('It is the', turnCounter, 'round.')
-  boardMove(boxId)
-}
-
-const boardMove = function (boxId) {
-  const xHtml = (`<p>X</p>`)
-  const oHtml = (`<p>O</p>`)
-  let move = ''
-  const boardCell = getData(boxId)
-  console.log(boardArray)
-  console.log(boardCell)
-  if (boardArray[boardCell] === '') {
-    console.log('board cell is', boardCell)
-    if (!(turnCounter % 2 === 0)) {
-      $(boxId).html(xHtml)
-      move = 'x'
-      console.log(move)
-      setMove(move, boardArray, boardCell)
-      turnCounter++
-    } else {
-      $(boxId).html(oHtml)
-      move = 'o'
-      console.log(move)
-      setMove(move, boardArray, boardCell)
-      console.log('o')
-      turnCounter++
-    }
-  } else {
-    console.log('Cell has been chosen!')
-  }
-}
-
+// Each individual board div has a click event attached upon load.
 $(() => {
   setAPIOrigin(location, config)
-  $('#0').on('click', clickEvent)
-  $('#1').on('click', clickEvent)
-  $('#2').on('click', clickEvent)
-  $('#3').on('click', clickEvent)
-  $('#4').on('click', clickEvent)
-  $('#5').on('click', clickEvent)
-  $('#6').on('click', clickEvent)
-  $('#7').on('click', clickEvent)
-  $('#8').on('click', clickEvent)
+  $('#0').on('click', events.clickEvent)
+  $('#1').on('click', events.clickEvent)
+  $('#2').on('click', events.clickEvent)
+  $('#3').on('click', events.clickEvent)
+  $('#4').on('click', events.clickEvent)
+  $('#5').on('click', events.clickEvent)
+  $('#6').on('click', events.clickEvent)
+  $('#7').on('click', events.clickEvent)
+  $('#8').on('click', events.clickEvent)
 })
 
 // use require with a reference to bundle the file and use it in this file
