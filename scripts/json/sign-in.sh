3c84e3820828c306b9bc925f25ef993cdd3d8afb
quidprocrow <windmillwarrior@gmail.com>
2017-12-21 14:59:32 -0500

    Verify sign out working
    Verify sign out working via curl script

--- scripts/json/sign-in.sh
+++ scripts/json/sign-in.sh
@@ -2,7 +2,7 @@
 
 # sh scripts/json/sign-in.sh
 
-curl "https://ga-library-api.herokuapp.com/sign-in" \
+curl "http://tic-tac-toe.wdibos.com/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
